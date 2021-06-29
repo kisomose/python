75dd99b5fbf07fe931983df6a816e1402d3ac389
tonny kisomose <kisomose1@gmail.com>
2021-06-29 12:19:30 +0300

    Add files via upload

---
+++ age calculator program.py
@@ -0,0 +1,10 @@
+print ('Answer the following questions?')
+name = input('Name ')
+age = int(input('Age '))
+#int means integer
+
+days = age *365
+minutes =age*525948
+seconds= age*31556926
+print(name,'you have lived for ',days,'days',minutes,'minutes',seconds,'seconds')
+print('thanks for using myApp')
